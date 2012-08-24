037d0cd930640b06c50eab5b640dc4ced08c58c5
Vytautas Astrauskas <vastrauskas@gmail.com>
2012-08-24 23:17:31 +0300

    Added wvdial configuration app.

---
+++ apps/wvdial/config.py
@@ -0,0 +1,9 @@
+{
+    'install': [
+        # TODO: 'wvdial',
+        ],
+    'templates': {
+        home('.wvdialrc'): 'wvdialrc',
+        scripts('connect-wap'): 'connect-wap',
+        },
+    }
