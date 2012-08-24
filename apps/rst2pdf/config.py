f9f0d1af7c797f958d12ee7437c51d9431a65a56
Vytautas Astrauskas <vastrauskas@gmail.com>
2012-08-24 23:16:15 +0300

    Added rst2pdf configuration app.

---
+++ apps/rst2pdf/config.py
@@ -0,0 +1,11 @@
+{
+    'install': [
+        # TODO: 'gnokii',
+        ],
+    'mkdir': [
+        home('.rst2pdf/styles'),
+        ],
+    'templates': {
+        home('.rst2pdf/styles/default.json'): 'default-style.json',
+        },
+    }
