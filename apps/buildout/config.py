1129f3a3fc2260f000444b6b3b28f996bc12e3f4
Vytautas Astrauskas <vastrauskas@gmail.com>
2012-08-25 10:59:08 +0300

    Added buildout configuration app.

---
+++ apps/buildout/config.py
@@ -0,0 +1,11 @@
+{
+    'install': [
+        ],
+    'mkdir': [
+        home('.buildout'),
+        home('.buildout/eggs'),
+        ],
+    'templates': {
+        home('.buildout/default.cfg'): 'default.cfg',
+        },
+    }
